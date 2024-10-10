b29419cad15ac9a8c3d5ae1e66defb26806bc6e2
Colin Neil <colin.neil18@gmail.com>
2024-10-09 22:58:03 -0400

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -15,25 +15,16 @@ require("vim-options")
 require("lazy").setup("plugins")
 
 vim.filetype.add({
-    extention = {
-        templ = "templ",
-    },
+	extention = {
+		templ = "templ",
+	},
 })
 
--- local NilocGroup = vim.api.nvim_create_augroup("NilocGroup", { clear = true })
--- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
---     pattern = "*",
---     group = NilocGroup,
---     callback = function()
---         vim.lsp.buf.format()
---     end,
--- })
-
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-    desc = 'Highlight when yanking (copyinng) text',
-    pattern = "*",
-    group = vim.api.nvim_create_augroup('niloc-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copyinng) text",
+	pattern = "*",
+	group = vim.api.nvim_create_augroup("niloc-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,25 @@
+return {
+	"AlexvZyl/nordic.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("nordic").load()
+		require("nordic").setup({
+			transparent = {
+				bg = false,
+			},
+			bright_border = true,
+			reduced_blue = true,
+			cursorline = {
+				theme = "dark",
+				blend = 0.55,
+			},
+		})
+
+		vim.cmd.colorscheme("nordic")
+	end,
+}
+
 -- return {
 --     "rebelot/kanagawa.nvim",
 --     lazy = false,
@@ -24,18 +46,18 @@
 --     end,
 -- }
 
-return {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        require("eldritch").setup({
-            styles = {
-                comments = { italic = false },
-                keywords = { italic = false },
-            },
-        })
-        vim.cmd.colorscheme("eldritch")
-    end,
-}
+-- return {
+--     "eldritch-theme/eldritch.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--         require("eldritch").setup({
+--             styles = {
+--                 comments = { italic = false },
+--                 keywords = { italic = false },
+--             },
+--         })
+--         vim.cmd.colorscheme("eldritch")
+--     end,
+-- }
